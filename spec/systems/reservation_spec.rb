4e44e77552e19bdfc906630771ed6d5b8b8207a4
yagi1231 <at.sao.1231@gmail.com>
2022-03-07 10:00:58 +0900

    spec/systemsボタン修正

--- spec/systems/reservation_spec.rb
+++ spec/systems/reservation_spec.rb
@@ -98,7 +98,7 @@ RSpec.describe 'Reservationsytems', js: true, type: :system do
     sign_in(user)
     visit reservations_path
     click_on "新規注文"
-    click_button "ヒレ丼   -780"
+    click_button "ヒレ丼-780"
   end
 
   scenario "ハンバーガーメニュー確認"do
